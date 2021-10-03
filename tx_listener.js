b5b1a157ace680aec66951faab09bc43e95d2100
ramgos <ramka2006@gmail.com>
2021-10-03 21:08:50 +0300

    simpler better gas price algorithm

--- tx_listener.js
+++ tx_listener.js
@@ -6,60 +6,32 @@ const config = require('./data/config.json');
 //*********************************\\
 
 const web3 = new Web3(config.WSS_RPC);
-const INTERVAL = 500;
-const MAX_RETRYS = 20;
-const MAX_PULLS = 5;
+const pendingTxHashToInfo = new Map();
 
-let usedPulls = 0;
-let locked = true;
-
-// lock subscription at the beggining for 15 seconds to drain all old pending transactions
-setTimeout(() => {
-    locked = false
-}, 15000);
-
-// whenever recieve pending transaction, if no more than MAX_PULLS transactions 
-// are being pulled, start pulling the transaction every INTERVAL 
 web3.eth.subscribe('pendingTransactions', (error, tx_hash) => {
-    if (locked) { return }
-    if (error) {
-        console.log("Unexpected error at beggining of event handling");
+    if (!error && tx_hash) {
+        web3.eth.getTransaction(tx_hash, (error, tx_data) => {
+            if (!error && tx_data) {
+                pendingTxHashToInfo.set(tx_hash, {
+                    timeSent: Date.now(),
+                    gasPrice: tx_data.gasPrice,
+                    timeTook: null
+                });
+            }
+        });
     }
-    const begginingTime = Date.now();
-    let endTime;
-    let gasPrice;
-    
-    if (usedPulls >= MAX_PULLS) {
-        return;
-    }
+});
 
-    new Promise(async (resolveTxGoingThrough, rejectTxGoingThrough) => {
-        usedPulls++;
-        let tries = 0;
-        while (tries < MAX_RETRYS) {
-            await new Promise((resolvePull) => {
-                setTimeout(() => {
-                    web3.eth.getTransaction(tx_hash, (error, tx_data) => {
-                        if (!tx_data || error || tx_data.blockNumber === null) {
-                            resolvePull();        
-                        }
-                        else {
-                            gasPrice = tx_data.gasPrice;
-                            endTime = Date.now();
-                            usedPulls--;
-                            resolveTxGoingThrough();
-                        }
-                    });
-                }, INTERVAL);
+web3.eth.subscribe('newBlockHeaders', (error, blockHeader) => {
+    if (!error && blockHeader) {
+        web3.eth.getBlock(blockHeader.hash, (error, blockData) => {
+            const timeNow = Date.now();
+            blockData.transactions.forEach(tx_hash => {
+                if (pendingTxHashToInfo.has(tx_hash)) {
+                    pendingTxHashToInfo.get(tx_hash).timeTook = timeNow - pendingTxHashToInfo.get(tx_hash).timeSent;
+                    console.log(pendingTxHashToInfo.get(tx_hash));
+                }                
             });
-            tries++;
-        }
-        usedPulls--;
-        rejectTxGoingThrough();
-
-    }).then(() => {
-        console.log(`${tx_hash} approved in ${(endTime - begginingTime) / 1000} with gas price of ${gasPrice}`);
-    }).catch(() => {
-        console.log(`${tx_hash} not approved in ${MAX_RETRYS} tries`);
-    });
+        });
+    }
 });
